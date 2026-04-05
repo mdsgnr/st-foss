fb96fc464c845a3f127770b63bbafa58e44f2a29
Moussa Mirad <78028575+mdsgnr@users.noreply.github.com>
2026-04-05 20:37:48 +0100

    Delete js directory

--- js/home.js
+++
@@ -1,8 +0,0 @@
-/* Home page specific JS */
-// Particle animation on hero
-document.addEventListener('DOMContentLoaded', () => {
-    // Animate orbit icons with a slight delay for stagger
-    document.querySelectorAll('.orbit-icon').forEach((icon, i) => {
-        icon.style.animationDelay = `${i * -3}s`;
-    });
-});
